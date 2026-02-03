78efc62e549b071b716c9076e0811309c0fd3bd6
Anusha Ch <72865744+Anusha3002@users.noreply.github.com>
2026-02-02 22:23:53 -0500

    Adding the server.js file of my sample application

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Cloud HW1 app running on EC2\n');
+});
+
+server.listen(80, () => {
+    console.log('Server running on port 80');
+});
